025e1c42eb756bf24434ebbbfe93ac3c727e5bf3
Slava-ZV <nadezhdadaniarova@yandex.ru>
2025-07-29 22:18:32 +0300

    app -> App

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -12,11 +12,11 @@ export const App = () => {
 			className={styles.main}
 			style={
 				{
-				'--font-family': articleState.fontFamilyOption.value,
-				'--font-size': articleState.fontSizeOption.value,
-				'--font-color': articleState.fontColor.value,
-				'--container-width': articleState.contentWidth.value,
-				'--bg-color': articleState.backgroundColor.value,
+					'--font-family': articleState.fontFamilyOption.value,
+					'--font-size': articleState.fontSizeOption.value,
+					'--font-color': articleState.fontColor.value,
+					'--container-width': articleState.contentWidth.value,
+					'--bg-color': articleState.backgroundColor.value,
 				} as CSSProperties
 			}>
 			<ArticleParamsForm
